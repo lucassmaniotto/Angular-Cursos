e9c04060e6b52b5a856930a621d596a9f5ff6744
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-02-14 11:15:58 -0300

    refactor: otimizando buscas com rxjs

--- Explore o Framework Angular/RxJS e Angular - programando de forma reativa/src/app/views/lista-livros/lista-livros.component.ts
+++ Explore o Framework Angular/RxJS e Angular - programando de forma reativa/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,37 +1,31 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap } from 'rxjs';
 import { Item } from 'src/app/models/interfaces/Item';
-import { Livro } from 'src/app/models/interfaces/Livro';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
+
+const DEBOUNCE_TIME = 300;
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent implements OnDestroy {
-  listaLivros: Livro[];
-  campoBusca: string = '';
-  subscription: Subscription;
-  livro: Livro;
+export class ListaLivrosComponent {
+  campoBusca = new FormControl();
 
   constructor(private livroService: LivroService) {}
 
-  buscarLivros() {
-    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next: (itens) => {
-        this.listaLivros = this.formatarLivros(itens);
-      },
-      error: (error) => console.error(error),
-    });
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(DEBOUNCE_TIME),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+    map((itens) => this.formatarLivros(itens))
+  );
 
   formatarLivros(itens: Item[]): LivroVolumeInfo[] {
     return itens.map((item) => new LivroVolumeInfo(item));
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
   }
 }
