9c598ee19dea17057eabd088714a382c62a8134b
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-03-28 11:49:07 -0300

    refactor: ajustando sobreposição de animações

--- Explore o Framework Angular/Angular - aprimore suas técnicas de animação e crie interfaces ainda mais atraentes/src/app/lista-tarefas/lista-tarefas.component.ts
+++ Explore o Framework Angular/Angular - aprimore suas técnicas de animação e crie interfaces ainda mais atraentes/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
@@ -14,6 +13,7 @@ import {
   shakeTrigger,
   shownStateTrigger,
 } from '../animations';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-tarefas',
@@ -26,7 +26,7 @@ import {
     filterTrigger,
     formButtonTrigger,
     flyInOutTrigger,
-    shakeTrigger
+    shakeTrigger,
   ],
 })
 export class ListaTarefasComponent implements OnInit {
@@ -38,6 +38,7 @@ export class ListaTarefasComponent implements OnInit {
   id: number = 0;
   campoBusca: string = '';
   tarefasFiltradas: Tarefa[] = [];
+  tarefasSubscription: Subscription = new Subscription();
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -49,16 +50,15 @@ export class ListaTarefasComponent implements OnInit {
 
   constructor(
     private service: TarefaService,
-    private router: Router,
     private fomBuilder: FormBuilder
   ) {}
 
-  ngOnInit(): Tarefa[] {
-    this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
-      this.tarefasFiltradas = listaTarefas;
+  ngOnInit(): void {
+    this.service.listar();
+    this.tarefasSubscription = this.service.tarefas$.subscribe((tarefas) => {
+      this.listaTarefas = tarefas;
+      this.tarefasFiltradas = tarefas;
     });
-    return this.tarefasFiltradas;
   }
 
   filtrarTarefasPorDescricao(descricao: string) {
@@ -86,23 +86,23 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   editarTarefa() {
-    this.service.editar(this.formulario.value).subscribe({
-      complete: () => this.atualizarComponente(),
-    });
+    if (this.formulario.valid) {
+      const tarefaEditada = this.formulario.value;
+      this.service.editar(tarefaEditada, true);
+      this.resetarFormulario();
+    }
   }
 
   criarTarefa() {
-    this.service.criar(this.formulario.value).subscribe({
-      complete: () => this.atualizarComponente(),
-    });
+    if (this.formulario.valid) {
+      const novaTarefa = this.formulario.value;
+      this.service.criar(novaTarefa);
+      this.resetarFormulario();
+    }
   }
 
-  excluirTarefa(id: number) {
-    if (id) {
-      this.service.excluir(id).subscribe({
-        complete: () => this.recarregarComponente(),
-      });
-    }
+  excluirTarefa(tarefa: Tarefa) {
+    if (tarefa.id) this.service.excluir(tarefa.id);
   }
 
   cancelar() {
@@ -119,15 +119,6 @@ export class ListaTarefasComponent implements OnInit {
     });
   }
 
-  recarregarComponente() {
-    this.router.navigate(['/listaTarefas']);
-  }
-
-  atualizarComponente() {
-    this.recarregarComponente();
-    this.resetarFormulario();
-  }
-
   carregarParaEditar(id: number) {
     this.service.buscarPorId(id!).subscribe((tarefa) => {
       this.formulario = this.fomBuilder.group({
@@ -141,19 +132,9 @@ export class ListaTarefasComponent implements OnInit {
     this.formAberto = true;
   }
 
-  finalizarTarefa(id: number) {
-    this.id = id;
-    this.service.buscarPorId(id!).subscribe((tarefa) => {
-      this.service.atualizarStatusTarefa(tarefa).subscribe(() => {
-        this.listarAposCheck();
-      });
-    });
-  }
-
-  listarAposCheck() {
-    this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.tarefasFiltradas = listaTarefas;
-    });
+  finalizarTarefa(tarefa: Tarefa) {
+    this.id = tarefa.id
+    this.service.atualizarStatusTarefa(tarefa)
   }
 
   habilitarBotao(): string {
